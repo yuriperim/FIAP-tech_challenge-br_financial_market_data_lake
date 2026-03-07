092ff41843a40f02c78a93dcba2cb9cec0593fa2
Yuri Perim <yuriperim@gmail.com>
2026-03-07 18:55:06 -0300

    feat: adiciona proteção contra acesso público ao bucket S3

--- infra/main.tf
+++ infra/main.tf
@@ -25,6 +25,15 @@ resource "aws_s3_bucket" "fiap_datalake" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket_security" {
+  bucket = aws_s3_bucket.fiap_datalake.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_object" "raw_path" {
   bucket = aws_s3_bucket.fiap_datalake.bucket
   key    = "raw/"
