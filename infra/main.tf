99cc2e97cc72cf361f4fa1eeb988bd858e69f204
Yuri Perim <yuriperim@gmail.com>
2026-03-14 15:55:32 -0300

    feat: adiciona role

--- infra/main.tf
+++ infra/main.tf
@@ -57,3 +57,76 @@ resource "aws_s3_object" "scripts_path" {
   bucket = aws_s3_bucket.fiap_datalake.bucket
   key    = "scripts/"
 }
+
+# IAM
+
+## IAM ROLES
+
+resource "aws_iam_role" "glue_etl_role" {
+  name = "glue-etl-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    infra_origin   = var.infra_origin
+    project_origin = var.project_origin
+    project_name   = var.project_name
+  }
+}
+
+## IAM POLICIES
+
+resource "aws_iam_policy" "glue_s3_access" {
+  name = "glue-s3-access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = aws_s3_bucket.fiap_datalake.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "${aws_s3_bucket.fiap_datalake.arn}/*"
+      }
+    ]
+  })
+
+  tags = {
+    infra_origin   = var.infra_origin
+    project_origin = var.project_origin
+    project_name   = var.project_name
+  }
+}
+
+## IAM POLICY ATTACHMENTS
+
+resource "aws_iam_role_policy_attachment" "glue_service_policy" {
+  role       = aws_iam_role.glue_etl_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3_attach" {
+  role       = aws_iam_role.glue_etl_role.name
+  policy_arn = aws_iam_policy.glue_s3_access.arn
+}
