277f949025f7c6d33e71bb086a457103f58da31b
Yuri Perim <yuriperim@gmail.com>
2026-03-15 23:14:53 -0300

    feat: adiciona job glue de extração, restando desenvolver script

--- infra/main.tf
+++ infra/main.tf
@@ -22,6 +22,20 @@ variable "project_name" {
   default     = "br_financial_market_data_lake"
 }
 
+variable "glue_default_arguments" {
+  description = "Argumentos padrão para o Glue"
+  type        = map(string)
+  default = {
+    "--job-language"                     = "python"
+    "--job-bookmark-option"              = "job-bookmark-disable"
+    "--enable-metrics"                   = "true"
+    "--enable-observability-metrics"     = "true"
+    "--enable-continuous-cloudwatch-log" = "false"
+    "--enable-spark-ui"                  = "true"
+    "--enable-glue-datacatalog"          = "true"
+  }
+}
+
 # S3
 
 resource "aws_s3_bucket" "fiap_datalake" {
@@ -55,7 +69,27 @@ resource "aws_s3_object" "refined_path" {
 
 resource "aws_s3_object" "scripts_path" {
   bucket = aws_s3_bucket.fiap_datalake.bucket
-  key    = "scripts/"
+  key    = "glue/scripts/"
+}
+
+resource "aws_s3_object" "extract_logs_path" {
+  bucket = aws_s3_bucket.fiap_datalake.bucket
+  key    = "glue/extract/spark-ui/"
+}
+
+resource "aws_s3_object" "extract_temp_path" {
+  bucket = aws_s3_bucket.fiap_datalake.bucket
+  key    = "glue/extract/temp/"
+}
+
+resource "aws_s3_object" "transform_logs_path" {
+  bucket = aws_s3_bucket.fiap_datalake.bucket
+  key    = "glue/transform/spark-ui/"
+}
+
+resource "aws_s3_object" "transform_temp_path" {
+  bucket = aws_s3_bucket.fiap_datalake.bucket
+  key    = "glue/transform/temp/"
 }
 
 # IAM
@@ -130,4 +164,42 @@ resource "aws_iam_role_policy_attachment" "glue_service_policy" {
 resource "aws_iam_role_policy_attachment" "glue_s3_attach" {
   role       = aws_iam_role.glue_etl_role.name
   policy_arn = aws_iam_policy.glue_s3_access.arn
+}
+
+# GLUE
+
+resource "aws_glue_job" "extract_job" {
+  name     = "br-financial-market-extract-job"
+  role_arn = aws_iam_role.glue_etl_role.arn
+
+  command {
+    name            = "glueetl" # Spark engine
+    python_version  = "3"
+    script_location = "s3://${aws_s3_bucket.fiap_datalake.bucket}/glue/scripts/extract_job.py"
+  }
+
+  default_arguments = merge(
+    var.glue_default_arguments,
+    {
+      "--spark-event-logs-path"     = "s3://${aws_s3_bucket.fiap_datalake.bucket}/glue/extract/spark-ui/"
+      "--TempDir"                   = "s3://${aws_s3_bucket.fiap_datalake.bucket}/glue/extract/temp/"
+      "--additional-python-modules" = "yfinance==0.2.66"
+    }
+  )
+
+  glue_version      = "4.0"
+  worker_type       = "G.1X"
+  number_of_workers = 2
+  max_retries       = 0
+  timeout           = 10 # tempo máximo de execução em minutos
+
+  execution_property {
+    max_concurrent_runs = 1 # previne execução concorrente (ex.: agendamento + execução manual console)
+  }
+
+  tags = {
+    infra_origin   = var.infra_origin
+    project_origin = var.project_origin
+    project_name   = var.project_name
+  }
 }
